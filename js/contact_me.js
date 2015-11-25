e82e0a094507311e916cd55c0aaa03ada39a2498
Guillermo Haddad <cejotagui@yahoo.com.br>
2015-11-25 13:24:40 -0200

    contato funfando

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -20,15 +20,6 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
                 cache: false,
                 success: function() {
                     // Enable button & show success message
